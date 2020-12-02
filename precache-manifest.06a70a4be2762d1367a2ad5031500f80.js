9996865d1771d2dcbefc0bfb6f2d8a638b13aa6f
ProstoStasyan <stanislavzebek@mail.ru>
2020-12-02 21:34:24 +0300

    TRY_5

---
+++ precache-manifest.06a70a4be2762d1367a2ad5031500f80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e940cbc253924965acea4505c2d432",
+    "url": "/React-demo/index.html"
+  },
+  {
+    "revision": "89fd14db733fc2e5b00b",
+    "url": "/React-demo/static/css/3.c9fb0d89.chunk.css"
+  },
+  {
+    "revision": "2b53416b8ab515d32281",
+    "url": "/React-demo/static/css/main.8ccc8dec.chunk.css"
+  },
+  {
+    "revision": "93353abbb0ccea78a432",
+    "url": "/React-demo/static/js/2.2e3001d6.chunk.js"
+  },
+  {
+    "revision": "89fd14db733fc2e5b00b",
+    "url": "/React-demo/static/js/3.137a8f62.chunk.js"
+  },
+  {
+    "revision": "2b53416b8ab515d32281",
+    "url": "/React-demo/static/js/main.692f81a1.chunk.js"
+  },
+  {
+    "revision": "d380ac6ba6c023546aa4",
+    "url": "/React-demo/static/js/runtime~main.492ef522.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/React-demo/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "d1cde82f52edd8b4305ee9cc13569e72",
+    "url": "/React-demo/static/media/tails.d1cde82f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-demo/precache-manifest.38c79326b85b5c83d64528da9adf29cc.js"
+  "/React-demo/precache-manifest.06a70a4be2762d1367a2ad5031500f80.js"
 );
 
 self.addEventListener('message', (event) => {
